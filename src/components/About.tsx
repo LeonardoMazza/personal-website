0313f63f0aeb29a6b6e3e3c6dedc334643132319
Leonardo Mazza <desenvolvedormazza@gmail.com>
2025-10-10 01:01:27 +0100

    adding components and basic info

---
+++ src/components/About.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className="my-8">
+      <h2 className="text-3xl font-bold">About Me</h2>
+      <p className="mt-4">
+        Front-end Developer with 7 years of professional experience, including 3+ years specializing in Vue.js. Skilled at building responsive, high-performance, user-centric interfaces integrated with REST APIs. Strong command of Vue 3, Composition API, Pinia, PrimeVue, TailwindCSS, and TypeScript.
+    </p>
+      <p className="mt-4">
+        I also bring full-stack value with expertise in Laravel/PHP, enabling seamless collaboration on backend integrations. Experienced in agile methodologies, tech leadership, and delivering quality across the development lifecycle.
+    </p>
+      <p className="mt-4">
+        Open to front-end opportunities (remote EU & Portugal) leveraging Vue.js, or hybrid/full-stack roles where my broader skill set can add value.
+      </p>
+    </section>
+  );
+};
+
+export default About;
---
+++ src/components/Header.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Introduction = () => {
+  return (
+    <section className="my-8">
+      <h1 className="text-4xl font-bold">Leonardo Mazza</h1>
+      <h2 className="text-1xl">Fullstack Developer</h2>
+      <p className="mt-4">
+        I am a fullstack developer with a passion for building web applications.
+      </p>
+    </section>
+  );
+};
+
+export default Introduction;
